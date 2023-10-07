f051a0e18fa67cd11ae2f1207a176cf331ffcd64
Kin Hin Wong <kinhinw@speechmatics.com>
2023-10-07 21:20:59 +0100

    Shorthand for bank switching

--- icm20948/icm20948.py
+++ icm20948/icm20948.py
@@ -12,11 +12,11 @@ class ICM20948Transport(I2CDevice):
 
     def set_bank(self, bank: int):
         if self._bank != bank:
-            self._write(Registers.BANK_SEL[1], bank << 4)
+            self._write(Registers.BANK_SEL.addr, bank << 4)
             self._bank = bank
 
     def get_bank(self) -> int:
-        cur_bank = self._read(Registers.BANK_SEL[1])
+        cur_bank = self._read(Registers.BANK_SEL.addr)
         cur_bank >>= 4
         self._bank = cur_bank
         return cur_bank
@@ -27,3 +27,11 @@ class ICM20948Transport(I2CDevice):
         self.set_bank(bank)
         yield
         self.set_bank(prev_bank)
+
+    def set_register(self, register: Registers, value: bytes):
+        with self.bank(register.bank):
+            self.write(register.addr, value)
+
+    def get_register(self, register: Registers):
+        with self.bank(register.bank):
+            return self.read(register.addr)
